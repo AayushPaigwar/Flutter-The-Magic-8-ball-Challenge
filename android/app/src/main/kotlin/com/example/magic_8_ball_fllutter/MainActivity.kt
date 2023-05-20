a16f9514203ecd443aba8b82a5aa4e70bf23c295
Aayush <aayush.paigwar123@gmail.com>
2023-05-20 17:11:44 +0530

    Challenge Completed

---
+++ android/app/src/main/kotlin/com/example/magic_8_ball_fllutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magic_8_ball_fllutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 1, 25, 44),
+          title: Center(child: Text("Ask Me Anything")),
+        ),
+        backgroundColor: Colors.blue,
+        body: Ball(),
+      ),
+    );
+  }
+}
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int changeQuestion= 2;
+  void changequestion(){
+    changeQuestion = Random().nextInt(5)+1;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  changequestion();
+                });
+              },
+              child: Image.asset('images/ball$changeQuestion.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
